8d62a561ae6af5fa74c33e31f309e4b7d4fa6218
Mathew Chan <mathewchan@Mathews-MacBook-Pro.local>
2020-05-27 22:03:27 -0400

    Bump version

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,4 @@
-/* @license v0.2.0 Author: Mathew Chan. Copyright ESQIDO LTD. All Rights Reserved. */
+/* @license v0.2.4 Author: Mathew Chan. Copyright ESQIDO LTD. All Rights Reserved. */
 
 // Define variables
 let initCount = 0
